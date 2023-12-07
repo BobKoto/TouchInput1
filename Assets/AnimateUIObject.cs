a9ea54f7ef6629ea271f2667f129ba227e2c8413
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-12-07 01:11:52 -0500

    Add UI anim.
    Set html canvas Hgt 710 Wdth 400.
    Build & move to web project for publishing

---
+++ Assets/AnimateUIObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateUIObject : MonoBehaviour
+{
+    public float rotateSpeed = 100;
+    public bool showDebug = false;
+
+    void Start()
+    {
+        StartCoroutine(WiggleUIObject());
+    }
+
+    IEnumerator WiggleUIObject()
+    {
+        var rotateAmount = 0f;
+        while (true)
+        {
+            if (ScoreKeeper.gameOver)
+            {
+                while (rotateAmount < .2f && ScoreKeeper.gameOver)    //.2f is brute forced cuz I don't know the value of rotation.z is determined 
+                {
+                    transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+                    rotateAmount = transform.rotation.z;
+                    if (showDebug) Debug.Log("rotateAmount = " + rotateAmount + "  t.rotation.z = " + transform.rotation.z);
+                    yield return null;
+                }
+                rotateAmount = 0;
+                if (showDebug) Debug.Log(" 1st rotate done now the other way?");
+                while (rotateAmount >= -.2f && ScoreKeeper.gameOver)
+                {
+                    transform.Rotate(0f, 0f, - (rotateSpeed * Time.deltaTime));
+                    rotateAmount = transform.rotation.z;
+                    yield return null;
+                }
+            }
+            yield return null;
+        }
+
+    }
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
